0ab51a517516a502ceb860dc5786b7c2aa607927
ajureel <ajureel@quickenloans.com>
2022-05-02 19:59:48 -0400

    item schema

--- server/models/Item.js
+++ server/models/Item.js
@@ -4,12 +4,44 @@ const dateFormat = require('../utils/dateFormat');
 const reactionSchema = require('./Reaction');
 
 const itemSchema = new Schema({
-    itemText: {
+    itemPublisher: { //****NEED TO MAKE THIS A LOOKUP */ 
+        // ie. DC, Marvel, Etc
+        type: String,
+        required: "You cant leave the publisher empty!",
+        minlength: 1
+    },
+    itemTitle: { //ie Batman, Wolverine, SuperMan vs Batman, etc.
+        type: String,
+        required: "You cant leave the Title empty!",
+        minlength: 1
+    },
+    itemIssueTitle: {
+        type: String,
+        required: "You cant leave the Issue Title empty!",
+        minlength: 1
+    },
+    itemIssueNumber: {
+        type: String,
+        required: "You cant leave the Issue Number empty!",
+        minlength: 1
+    },
+    itemDescription: {
+        type: String,
+        required: "You cant leave the item description empty!",
+        minlength: 1
+    },
+    itemCondition: { //****NEED A LOOKUP */
+        //ie. (Poor, Fair, Good, Very Fine, Mint)
+        type: String,
+        required: "You cant leave the item condition empty!",
+        minlength: 1
+    },
+    itemPrice: { //****SET TO DECIMAL */
         type: String,
         required: "You cant leave the item empty!",
         minlength: 1
     },
-    itemImage: {
+    itemImage: { //Gets set on upload
         type: String
     },
     createdAt: {
@@ -17,7 +49,7 @@ const itemSchema = new Schema({
         default: Date.now,
         get: timestamp => dateFormat(timestamp)
     },
-    username: {
+    username: { //***Need to link to user table */
         type: String,
         required: true
     },
