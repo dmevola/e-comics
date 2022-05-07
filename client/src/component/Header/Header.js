b9c61c508253a9d261f72318636b75e629ab09ff
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-05-07 14:48:21 -0500

    started header styling

--- client/src/component/Header/Header.js
+++ client/src/component/Header/Header.js
@@ -7,6 +7,8 @@ import { useStateValue } from '../../StateProvider';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 import { useQuery } from '@apollo/client';
 import { QUERY_ME_BASIC } from '../../utils/queries';
+import { HiSearch } from 'react-icons/hi'
+import { ImSearch } from 'react-icons/im'
 
 export default function Header() {
     const [{basket,user},dispatch] = useStateValue();
@@ -16,13 +18,14 @@ export default function Header() {
         Auth.logout();
       };
     return (
-        <div className="header">
+        <div className="header bg-blue-400 fixed w-full h-[80px] flex justify-between items-center">
            <Link underline="none" to="/">
-                <h4 className="header__logo" alt="">E-comic</h4>
+                <h4 className="header__logo text-2xl text-yellow-300 hover:text-red-300" alt="">E-comic</h4>
            </Link>
            <div className="header__search">
-                <input className="header__searchInput" type="text"/>
-                <SearchIcon className="header__searchIcon"/> 
+                <input className="header__searchInput max-w-[400px]" type="text"/>
+                <button className='<button class="btn inline-block p-1.5  bg-red-400 font-medium leading-tight uppercase rounded shadow-md hover:bg-yellow-200 hover:shadow-lg   focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out items-center'>{<ImSearch size={15} className='text-gray-600 hover:text-red-400' />}</button>
+                {/* <SearchIcon className="header__searchIcon"/>  */}
                 {/* {userData.me.username} */}
                  
             </div>
