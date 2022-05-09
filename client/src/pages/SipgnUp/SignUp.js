44892b52feb54b034c1f94997d1fa69753c8eadf
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-05-09 06:15:07 -0500

    styled Sign-up page

--- client/src/pages/SipgnUp/SignUp.js
+++ client/src/pages/SipgnUp/SignUp.js
@@ -41,18 +41,18 @@ export default function Signup() {
 
 
   return (
-    <div className="signUp">
+    <div className="signUp my-0 bg-yellow-200 flex">
         {/* <Link to='/'>
             <img src='https://www.freepnglogos.com/uploads/amazon-png-logo-vector/woodland-gardening-amazon-png-logo-vector-8.png'
                     className="signUp__logo" alt=""></img>
         </Link> */}
 
-        <div className="signUp__container">
-            <h1>sign-in</h1>
-            <form onSubmit={handleFormSubmit}>
+        <div className="signUp__container my-7 py-0 pb-6 flex bg-blue-300 justify-center items-center">
+            <h1 className='font-bold'>Create Account</h1>
+            <form onSubmit={handleFormSubmit} className='flex-col sm:w-[250px] mx-9 items-center flex'>
                 <h5>Username</h5>
                 <input
-                  className='form-input'
+                  className='form-input rounded pl-2 pr-0 text-center'
                   placeholder='Your username'
                   name='username'
                   type='username'
@@ -61,20 +61,24 @@ export default function Signup() {
                   onChange={handleChange}
                 />
                 <h5>E-mail</h5>
-                <input placeholder='Your email'
+                <input 
+                    className='rounded pl-2 pr-0 text-center'
+                    placeholder='Your email'
                     name='email'
                     type='email'
                     id='email'
                     value={formState.email}
                     onChange={handleChange} />
                 <h5>Password</h5>
-                <input  placeholder='******'
+                <input  
+                    className='rounded pl-2 pr-0 text-center'
+                    placeholder='******'
                     name='password'
                     type='password'
                     id='password'
                     value={formState.password}
                     onChange={handleChange}/>
-                <button className="signUp__registerButton" >Create your E-Comic account</button>
+                <button className="signUp__registerButton btn p-1.5 font-bold text-gray-800 bg-green-300 leading-tight uppercase rounded shadow-md hover:bg-purple-200 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out items-center sm:px-3" >Create Account</button>
             </form>
             {error && <h1>Login failed</h1>}
             <p>
