83bca0e2399a128506cafc3e711e513c946f74e2
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-05-09 06:08:38 -0500

    styled Sign In

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -48,30 +48,32 @@ export default function Login() {
   }
 
   return (
-    <div className="login">
+    <div className="login my-0 bg-yellow-200 flex">
         {/* <Link to='/'>
             <img src='https://www.freepnglogos.com/uploads/amazon-png-logo-vector/woodland-gardening-amazon-png-logo-vector-8.png'
                     className="login__logo" alt=""></img>
         </Link> */}
 
-        <div className="login__container">
-            <h1>sign-in</h1>
-            <form onSubmit={handleFormSubmit}>
+        <div className="login__container my-7 py-0 pb-6 flex bg-blue-300 justify-center items-center">
+            <h1 className='font-bold'>Sign-In</h1>
+            <form onSubmit={handleFormSubmit} className='flex-col sm:w-[250px] mx-9 items-center flex'>
                 <h5>E-mail</h5>
-                <input placeholder='Your email'
+                <input className='rounded pl-2 pr-0 text-center'
+                    placeholder='Your email'
                     name='email'
                     type='email'
                     id='email'
                     value={formState.email}
                     onChange={handleChange} />
                 <h5>Password</h5>
-                <input  placeholder='******'
+                <input className='rounded pl-2 pr-0 text-center'
+                    placeholder='******'
                     name='password'
                     type='password'
                     id='password'
                     value={formState.password}
                     onChange={handleChange}/>
-                <button className="login__signInButton" >Sign In</button>
+                <button className='btn p-1.5 font-bold text-gray-800 bg-red-400 leading-tight uppercase rounded shadow-md hover:bg-yellow-200 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out items-center sm:px-3'>Sign In</button>
             </form>
             {error && <h1>Login failed</h1>}
             <p>
@@ -79,7 +81,7 @@ export default function Login() {
                 Notince, our Cookies Notince and our Interest-based Ads 
                 Notice.
             </p>
-            <button className="signUp__registerButton" onClick={signUp}>Create your E-Comic account</button>
+            <button className="signUp__registerButton btn p-1.5 font-bold text-gray-800 bg-green-300 leading-tight uppercase rounded shadow-md hover:bg-purple-200 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out items-center sm:px-3" onClick={signUp}>Create your E-Comic account</button>
         </div>
     </div>
   )
