a8babd5c81398383662483997c48a62ef7acc915
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-05-09 05:23:19 -0500

    styled home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,22 +5,18 @@ import ComicList from '../../component/ComicList/Index'
 
 function Home() {
     return (
-        <div className="home">
-            <div className="home__container">
-                    <img className="home__Image" src="https://m.media-amazon.com/images/P/172919785X.01._SCLZZZZZZZ_SX500_.jpg" alt="" />
+        <div className="home w-full md:h-screen pt-12 md:pt-4 flex items-center justify-center">
+            <div className="home__container w-full pt-5 items-center h-full grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+                    {/* <img className="home__Image fixed-top flex" src="https://m.media-amazon.com/images/P/172919785X.01._SCLZZZZZZZ_SX500_.jpg" alt="" /> */}
                 
-                <div className="home__row">
+                <div className="home__row max-w-[1300px] justify-center">
                     <Product id = "3344" title='Cat Kid Comic Club: A Graphic Novel (Cat Kid Comic Club #1): From the Creator of Dog Man' price={7.00} image='https://images-na.ssl-images-amazon.com/images/I/51wuQxpeDoL._SX336_BO1,204,203,200_.jpg' rating={4} />
-                    <Product  id = "2323223" title='Cat Kid Comic Club #4: A Graphic Novel: From the Creator of Dog Man' price={22.00} image="https://m.media-amazon.com/images/I/615Lif3nhNL._AC_UY436_FMwebp_QL65_.jpg" rating={5} />
+                </div>
+                <div className="home__row max-w-[1300px]">
+                    <Product id = "23234223" title='Marvel Greatest Comics: 100 Comics that Built a Universe, ' price={11.99} image='https://m.media-amazon.com/images/I/A1tmfYAtb+L._AC_UY436_FMwebp_QL65_.jpg' rating={5} />    
                 </div>
                 <div className="home__row">
-                <Product id = "23234223" title='Marvel Greatest Comics: 100 Comics that Built a Universe, ' price={11.99} image='https://m.media-amazon.com/images/I/A1tmfYAtb+L._AC_UY436_FMwebp_QL65_.jpg' rating={5} />
-                <Product id = "2323276523" title='Blank Comic Book: Large Cartoon Notebooks for Kids 9-12 with Variety of Templates Draw Your Own Comics ' price={100.00} image='https://m.media-amazon.com/images/I/71ciofnl5ZL._AC_UY436_FMwebp_QL65_.jpg' rating={5} />
-                <Product id = "232328723" title='Blank Comic Book: Large Cartoon Notebooks for Kids 9-12 with Variety of Templates Draw Your Own Comics ,' price={69.99} image='https://m.media-amazon.com/images/I/71U3oaeBW6L._AC_UY436_FMwebp_QL65_.jpg' rating={5} />
-                    
-                </div>
-                <div className="home__row">
-                <Product id = "232653223" title='Blank Manga Template,' price={119.00} image='https://m.media-amazon.com/images/I/51RHU3JmqcL._AC_UY436_FMwebp_QL65_.jpg' rating={5} />
+                    <Product id = "232653223" title='Blank Manga Template,' price={119.00} image='https://m.media-amazon.com/images/I/51RHU3JmqcL._AC_UY436_FMwebp_QL65_.jpg' rating={5} />
                 </div>
                 <ComicList></ComicList>
             </div>
