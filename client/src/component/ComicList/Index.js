c99091e9f2002334bf7ad8ab9081871496f5a9b8
dmevola <dmevola@gmail.com>
2022-05-08 23:22:11 -0400

    updated img size to 450 w

--- client/src/component/ComicList/Index.js
+++ client/src/component/ComicList/Index.js
@@ -9,18 +9,19 @@ const ComicList = ({ items, title }) => {
       <h3>{title}</h3>
       {items && 
       items.map(item => (
-      <div key={item._id}>
+      <div key={item._id} className='home__row'>
         <p>
-          created at {item.createdAt}
+          Listed at {item.createdAt} 
+          <br/> by {item.username}
           </p>
           <div>
+            <p><strong>Title:</strong> {item.itemTitle}</p>
             <p><strong>Issue Title:</strong> {item.itemIssueTitle}</p>
             <p><strong>Description:</strong>{item.itemDescription}</p>
             <p><strong>Publisher:</strong>{item.itemPublisher}</p>
-            <p><strong>Seller:</strong>{item.username}</p>
             <p><strong>Price:</strong>${item.itemPrice}</p>
             <p><strong>Condition:</strong> {item.itemCondition}</p>
-            {/* <img src={item.itemImage}/> */}
+            <img src={item.itemImage} className='comic_img'/>
           </div>
       </div>
 
